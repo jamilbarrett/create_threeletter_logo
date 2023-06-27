4d482f5f5f7e8cd275f368ef8ac9abd4c516c679
Jamil Barrett <jamilbarrett@jamils-imac.myfiosgateway.com>
2023-06-26 20:53:06 -0400

    created class, file, prompts

--- index.js
+++ index.js
@@ -2,22 +2,27 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 
-class shape {
+class Shape {
   constructor(cirlce, square, triangle) {
-    this.circle
-
+    this.circle = cirlce
+    this.square = square
+    this.triangle = triangle
   }
 }
 
 
-
-
-
-
+// Prompt user for input
 const userInput = [
   {
     name: 'three',
-    message: 'Please enter up to 3 characters'
+    message: 'Please enter 3 characters',
+    validate: (input) => {
+      if (input.length > 3 || input.length< 3) {
+        return 'Please enter 3 characters';
+      } else {
+        return true
+      }
+    }
   },
   {
     name: 'text color',
@@ -27,18 +32,18 @@ const userInput = [
     type: 'list',
     name: 'shape',
     message: 'Please select the desired shape',
-    choices: ['Circle', 'Square', 'Triangle']
+    choices: ['cirlce', 'square', 'triangle']
   },
   {
     name: 'shape color',
     message: 'Please select the color of your chosen shape'
   }
 ];
-
+// writes user input to svg file
 inquirer.prompt(userInput)
   .then((answers) => {
     fs.writeFile('./logo.svg', JSON.stringify(answers), 'utf8', (err) => {
       if (err) throw err;
-      console.log('Logo.SVG file has been created successfully.');
+      console.log('Generated logo.svg');
     });
   })
